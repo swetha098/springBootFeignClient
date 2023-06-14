3870c61a8a079c3132516e57b52dce7ce1d91fec
Sasi <lakursasi@gmail.com>
2023-06-14 18:11:11 +0530

    feign

---
+++ src/main/java/com/UST/SpringBootFeignClient/SpringBootFeignClientApplication.java
@@ -0,0 +1,15 @@
+package com.UST.SpringBootFeignClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootFeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFeignClientApplication.class, args);
+	}
+
+}
